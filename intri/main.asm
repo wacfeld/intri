d747c082c7c1cbce3618a65343787bad438c1643
wacfeld <wacfeldwang333@gmail.com>
2022-05-11 01:02:29 -0400

    bin_str

--- intri/main.asm
+++ intri/main.asm
@@ -42,6 +42,10 @@ faillen equ $ - failmsg
 successmsg db "passes test", 10
 successlen equ $ - successmsg
 
+onechar db "1"
+zerchar db "0"
+nlchar db 10
+
 section .bss
 c rest 3
 temp111 rest 300
@@ -56,7 +60,65 @@ temp222 rest 300
 ; p3 rest 3
 
 section .text
-global _start
+
+;------------------------------------------------------------------------------
+; PROCEDURE:    unpack
+; IN:           edx:eax: qword
+; OUT:          rax:qword
+; MODIFIES:     rax, rdx
+; CALLS:        
+; DETAILS:      
+%macro unpack 0
+  shl rdx, 32 ; edx -> left half of rdx
+  ; and rax, 0FFFF_FFFFH ; zero left half of rax
+  ; zero left half of rax
+  shl rax, 32
+  shr rax, 32
+  add rax, rdx ; whole number in rdx now
+%endmacro
+
+;------------------------------------------------------------------------------
+; PROCEDURE:    bin_str
+; IN:           rax: qword binary number
+; OUT:          none
+; MODIFIES:     registers
+; CALLS:        system call
+; DETAILS:      checks each bit, then prints corresponding ascii 0 or 1, followed by newline at very end
+%macro bin_str 0
+        mov r8, rax ; we need rax, so store elsewhere
+
+        mov r9,63  ; run 63+1 = 64 times
+shift:  shl r8,1   ; shift rightmost bit into carry flag
+        jc .one     ; if bit is 1, jump
+
+; print 0
+.zer:   mov rax, 4
+        mov rbx, 1
+        mov rcx, zerchar
+        mov rdx, 1
+        int 80h
+        jmp .cloop              ; jump to loop check
+
+; print 1
+.one    mov rax, 4
+        mov rbx, 1
+        mov rcx, onechar
+        mov rdx, 1
+        int 80h
+        jmp .cloop              ; jump to loop check
+
+; loop check
+.cloop: dec r9                 ; dec bit counter
+        cmp r9,0               ; compare bit counter with 0
+        jge shift               ; if (signed) greater, equal 0, loop
+
+; print newline
+        mov rax, 4
+        mov rdx, 1
+        mov rcx, nlchar
+        mov rdx, 1
+        int 80h
+%endmacro
 
 ;------------------------------------------------------------------------------
 ; PROCEDURE:    norm
@@ -369,7 +431,19 @@ samesign:
 ; TODO make everything macros
 ; TODO optimize
 
+global _start
 _start:
+  rdtsc
+  unpack
+  mov r8, rax
+  
+
+  rdtsc
+  unpack
+  sub rax, r9
+  bin_str
+  jmp .exit
+
   finit ; reset fpuregs
 
   vload p ; p
